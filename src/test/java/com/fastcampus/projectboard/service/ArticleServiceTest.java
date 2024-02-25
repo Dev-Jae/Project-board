0ba0109db5c8d1be92451595a1f54cd479f049a1
Dev-Jae <devhello940112@gmail.com>
2024-02-25 23:29:23 +0900

    #30 - 회원 계정 도메인 변경사항을 영향받는 코드에 적용
    `id` -> `userId`로 id를 변경하는 내용을
    모든 자바코드에 반영

--- src/test/java/com/fastcampus/projectboard/service/ArticleServiceTest.java
+++ src/test/java/com/fastcampus/projectboard/service/ArticleServiceTest.java
@@ -259,7 +259,6 @@ class ArticleServiceTest {
 
     private UserAccountDto createUserAccountDto() {
         return UserAccountDto.of(
-                1L,
                 "todd",
                 "password",
                 "todd@mail.com",
